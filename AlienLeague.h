913c7cfa5ed03bdef4c642ac3b349d309b3d4fa5
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:34:07 +0100

    Initial import

---
+++ AlienLeague.h
@@ -0,0 +1,29 @@
+#ifndef _ALIENLEAGUE_H
+#define _ALIENLEAGUE_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t AlienLeague10[];
+    extern const uint8_t AlienLeague11[];
+    extern const uint8_t AlienLeague12[];
+    extern const uint8_t AlienLeague13[];
+    extern const uint8_t AlienLeague14[];
+    extern const uint8_t AlienLeague15[];
+    extern const uint8_t AlienLeague16[];
+    extern const uint8_t AlienLeague17[];
+    extern const uint8_t AlienLeague18[];
+    extern const uint8_t AlienLeague19[];
+    extern const uint8_t AlienLeague20[];
+    extern const uint8_t AlienLeague21[];
+    extern const uint8_t AlienLeague22[];
+    extern const uint8_t AlienLeague23[];
+    extern const uint8_t AlienLeague24[];
+    extern const uint8_t AlienLeague25[];
+    extern const uint8_t AlienLeague26[];
+    extern const uint8_t AlienLeague27[];
+    extern const uint8_t AlienLeague28[];
+    extern const uint8_t AlienLeague29[];
+    extern const uint8_t AlienLeague30[];
+};
+#endif
